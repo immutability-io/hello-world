dad00b5b5237a9e023c8dfa22153917f21e1ae38
Jeff Ploughman <jeff_ploughman@troweprice.com>
2017-01-20 12:29:25 -0500

    Use env var

--- server.go
+++ server.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"crypto/tls"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/labstack/gommon/log"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 var (
@@ -14,6 +17,8 @@ var (
 		KeyLookup: "cookie:token",
 		Validator: validator,
 	}
+
+	ciamDomain = os.Getenv("CIAM_DOMAIN")
 )
 
 func defaultSkipper(c echo.Context) bool {
@@ -22,7 +27,28 @@ func defaultSkipper(c echo.Context) bool {
 
 func validator(key string, c echo.Context) bool {
 	c.Logger().Debug("Key " + key)
-	return key == "valid-key"
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	sessionRequest, _ := http.NewRequest(echo.GET, "https://"+ciamDomain+"/ui/api/session/verify", nil)
+	cookie := http.Cookie{Name: "token", Value: key, Domain: ciamDomain, Secure: true, HttpOnly: true}
+	sessionRequest.AddCookie(&cookie)
+	resp, err := client.Do(sessionRequest)
+	if resp != nil && resp.StatusCode == 200 {
+		sessionDetails, _ := http.NewRequest(echo.GET, "https://"+ciamDomain+"/ui/api/session", nil)
+		sessionDetails.AddCookie(&cookie)
+		resp, err = client.Do(sessionDetails)
+		if resp != nil {
+			htmlData, _ := ioutil.ReadAll(resp.Body)
+			c.Logger().Debug(string(htmlData))
+		}
+	}
+
+	if err != nil {
+		c.Logger().Debug(err)
+	}
+	return err == nil
 }
 
 func main() {
